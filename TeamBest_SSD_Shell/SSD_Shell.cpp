9e3c9616ac2fd407ff7f9570f4c9b1479ba89d33
Youngjo Park <anyways5188@gmail.com>
2025-05-09 18:17:31 +0900

    [Feature] Shell Program Run method 추가 1. 변경점 - Shell 을 위한 Run함수 추가 (무한Loop 이나 현재는 5회 loop후 탈출)
    2.검증
    [X] Build
    [X] Regression Test Pass
    [X] 해당 기능 TC (CheckRunMethod)

--- TeamBest_SSD_Shell/SSD_Shell.cpp
+++ TeamBest_SSD_Shell/SSD_Shell.cpp
@@ -1,7 +1,32 @@
 #include "ISSD_Shell.h"
+#include <iostream>
+
+using namespace std;
 
 class SSDShell : public IShell{
 public:
+
+    void Run(void) {
+        int loopCount = 0;
+        std::string line;
+        while (loopCount < 5) {
+            // 1. print command cursor
+            std::cout << "Shell>";
+
+            // 2. input command
+         // TODO : Enable later with Exit
+         //   std::getline(std::cin, line);
+         //   std::istringstream iss(line);
+
+            // 3. parsing command
+            // 3-1. invalid check
+            // 4. process command
+
+            //99. test code for loop count
+            cout << "loop count :" << loopCount++ << endl;
+        }
+    }
+
     void PrintHelp() override {}
     std::string ReadInputFile(int address) override { return 0x0; }
     bool WriteOutputFile(std::string output, int address) override { return true; }
