a5934f9861de71df1dcd3bfdbbd0b342370c9547
Youngjo Park <anyways5188@gmail.com>
2025-05-09 18:27:36 +0900

    [Refactor] Shell Program Interface 정리 1. 변경점 - 불필요한 Public Interface 정리 - Interface 이름 변경
    2.검증
    [X] Build
    [X] Regression Test Pass
    [  ] 해당 기능 TC

--- TeamBest_SSD_Shell/SSD_Shell.cpp
+++ TeamBest_SSD_Shell/SSD_Shell.cpp
@@ -1,35 +1,22 @@
-#include "ISSD_Shell.h"
-#include <iostream>
+#include "SSD_Shell.h"
 
-using namespace std;
+void SSDShell::Run(void) {
+    int loopCount = 0;
+    std::string line;
+    while (loopCount < 5) {
+        // 1. print command cursor
+        std::cout << "Shell>";
 
-class SSDShell : public IShell{
-public:
+        // 2. input command
+        // TODO : Enable later with Exit
+     //   std::getline(std::cin, line);
+     //   std::istringstream iss(line);
 
-    void Run(void) {
-        int loopCount = 0;
-        std::string line;
-        while (loopCount < 5) {
-            // 1. print command cursor
-            std::cout << "Shell>";
+        // 3. parsing command
+        // 3-1. invalid check
+        // 4. process command
 
-            // 2. input command
-         // TODO : Enable later with Exit
-         //   std::getline(std::cin, line);
-         //   std::istringstream iss(line);
-
-            // 3. parsing command
-            // 3-1. invalid check
-            // 4. process command
-
-            //99. test code for loop count
-            cout << "loop count :" << loopCount++ << endl;
-        }
+        //99. test code for loop count
+        cout << "loop count :" << loopCount++ << endl;
     }
-
-    void PrintHelp() override {}
-    std::string ReadInputFile(int address) override { return 0x0; }
-    bool WriteOutputFile(std::string output, int address) override { return true; }
-    void ProcessInputCommand(std::string command) override {}
-    bool ProcessParseInvalid(std::string command) override { return true; }
-};
+}
