1dc0ecc2e9b85b48c4a57f3e42abdcc18f0ed1d0
Youngjo Park <anyways5188@gmail.com>
2025-05-09 16:14:43 +0900

    [Feature] ShellProgram Instance 생성 - 변경점 ++ Shell Progrma Instance를 생성하고, 일부 인터페이스를 변경하였습니다.
    - 검증
    
    [X] Build
    
    [ ] Regression Test Pass
    
    [X] 해당 기능 TC

---
+++ TeamBest_SSD_Shell/ISSD_Shell.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+class IShell {
+public:
+    virtual void PrintHelp() = 0;
+    virtual std::string ReadInputFile(int address) = 0; // File Read
+    virtual bool WriteOutputFile(std::string output, int address) = 0;  // File Write
+    virtual void ProcessInputCommand(std::string command) = 0;  // Command Input Processing
+    virtual bool ProcessParseInvalid(std::string command) = 0;  // Command Parsing & invalid Ã³¸®
+};
---
+++ TeamBest_SSD_Shell/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "gmock/gmock.h"
+#include "SSD_Shell.cpp"
+
+using namespace testing;
+
+TEST(ShellTS, CraeteShellInstance) {
+    EXPECT_NO_THROW(SSDShell* ssdShell = new SSDShell());
+}
+
+int main()
+{
+    ::testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
+}
