5958b7d8b1944fa8313ad87abb55f048a813f2cf
Wonseok KIm <sokaman@naver.com>
2025-05-09 15:48:15 +0900

    Private 에 정의된 변수화  함수 삭제 완료.

--- TeamBest_SSD_Shell/TeamBest_SSD_Shell.cpp
+++ TeamBest_SSD_Shell/TeamBest_SSD_Shell.cpp
@@ -11,11 +11,6 @@ public:
     virtual bool WriteOutputFile(std::string output, int address) = 0;  // File Write
     virtual void ProcessInputCommand(std::string command) = 0;  // Command Input Processing
     virtual bool ProcessParseInvalid(std::string command) = 0;  // Command Parsing & invalid 처리
-
-private:
-    const int MAX_LBA = 100;
-    bool IsValidLba(int lba);  // LBA 유효성 검사
-    bool IsValidValue(const std::string& value);  // VALUE 유효성 검사: 0x + 8자리 HEX
 };
 
 
