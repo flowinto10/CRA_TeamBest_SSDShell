3fdbdca77788ac7329245eacffc28293890ad5b5
eunshil.jeon <eunshil.jeon@gmail.com>
2025-05-09 18:18:20 +0900

    [Feature] SSD Shell 의 read 기능 추가 구현
    변경점 : ssd_nand.txt 에서 lba(address) 에 해당하는 값을 읽어서 string 을 뽑아 shell 응답 값을 만들어줌
    
    검증
    
    [ v] Build
    
    [ v] Regression Test Pass
    
    [ v] 해당 기능 TC

--- TeamBest_SSD_Shell/SSD_Shell.cpp
+++ TeamBest_SSD_Shell/SSD_Shell.cpp
@@ -1,10 +1,16 @@
 #include "ISSD_Shell.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <cstdlib>  // system 함수
+#include <sstream>  // ostringstream
+
 using namespace std;
 
-class SSDShell : public IShell{
+class SSDShell : public IShell {
 public:
-    void PrintHelp() override {
+	void PrintHelp() override {
 		cout << "SSD Shell Help" << endl;
 		cout << "  command list : read, write, fullread, fullwrite, exit, help\n\n";
 		cout << "  ---- usage ----" << endl;
@@ -17,9 +23,46 @@ public:
 		cout << "  exit" << endl;
 		cout << "  help" << endl;
 		cout << "  ----------------" << endl;
-    }
-    std::string ReadInputFile(int address) override { return 0x0; }
-    bool WriteOutputFile(std::string output, int address) override { return true; }
-    void ProcessInputCommand(std::string command) override {}
-    bool ProcessParseInvalid(std::string command) override { return true; }
+	}
+
+	string ReadInputFile(int address) override {
+		// todo : 나중에 실제로 구현된 ssd.exe 를 가지고 구현할 때는 이렇게?? 해야할 것 같아서 todo 남김
+		//int result = system("ssd.exe");
+
+		//ostringstream commandStream;
+		//commandStream << "ssd.exe " << address;
+		//string command = commandStream.str();
+
+		//// 2. 명령 실행
+		//int result = system(command.c_str());
+		//if (result != 0) {
+		//	cerr << "ssd.exe 실행 실패. 종료 코드: " << result << endl;
+		//	return;
+		//}
+
+
+		ifstream inputFile(SSD_NAND);
+		string buffer;
+		int lineNumber = -1;
+		string targetData;
+		while (getline(inputFile, buffer)) {
+			++lineNumber;
+			if (lineNumber == address) {
+				targetData = buffer;
+				break;
+			}
+		}
+		inputFile.close();
+		string result = "[Read] LBA " + to_string(address) + " : " + targetData;
+		cout << result << endl;
+		return result;
+	}
+
+	bool WriteOutputFile(std::string output, int address) override { return true; }
+	void ProcessInputCommand(std::string command) override {}
+	bool ProcessParseInvalid(std::string command) override { return true; }
+
+private:
+	const string SSD_NAND = "ssd_nand.txt"; // SSD NAND 파일 이름
+	const string SSD_OUTPUT = "ssd_output.txt"; // SSD 출력 파일 이름
 };
--- TeamBest_SSD_Shell/test_ssd_shell.cpp
+++ TeamBest_SSD_Shell/test_ssd_shell.cpp
@@ -1,13 +1,60 @@
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "ISSD_Shell.h"
 #include "SSD_Shell.cpp"
 
 using namespace std;
 using namespace testing;
 
+class ShellFixture : public Test {
+public:
+	const string SSD_NAND = "ssd_nand.txt"; // SSD NAND 파일 이름
+	const string SSD_OUTPUT = "ssd_output.txt"; // SSD 출력 파일 이름
+
+	bool writeToFile(const string& filename, const string& data) {
+		ofstream outputFile(filename);
+		if (!outputFile) {
+			cerr << "Error opening file for writing: " << filename << endl;
+			return false;
+		}
+		outputFile << data;
+		outputFile.close();
+		return true;
+	}
+
+	string readFromFile(const string& filename) {
+		ifstream inputFile(filename);
+		if (!inputFile) {
+			cerr << "Error opening file for reading: " << filename << endl;
+			return "";
+		}
+		string data((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+		inputFile.close();
+		return data;
+	}
+};
+
 TEST(ShellTest, PrintTC01) {
 	SSDShell* shell = new SSDShell();
 	shell->PrintHelp();
 	EXPECT_TRUE(true); // 출력 되는것만 본다
+}
+
+TEST_F(ShellFixture, ReadSSDShell_tc01) {
+	int lba = 0;
+	string expectedStr = "0x00000000";
+
+	// precondition
+	fstream ssdNandFile(SSD_NAND);
+	for (int i = 0; i < 100; ++i) {
+		ssdNandFile << expectedStr << endl; // 100 줄을 모두 0x00000000으로 초기화
+	}
+	ssdNandFile.close();
+
+	SSDShell* shell = new SSDShell();
+	string actualStr = shell->ReadInputFile(lba);
+	EXPECT_EQ(actualStr, "[Read] LBA 0 : " + expectedStr);
 }
