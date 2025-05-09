41816b7a23307e84262e1f9de43e126eb2c50763
Wonseok KIm <sokaman@naver.com>
2025-05-09 15:40:39 +0900

    parameter 이름 변경 ( input -> command),  private 에 작성된 함수를 virtual 함수에서 일반함수로 변경 완료.

--- TeamBest_SSD_Shell/TeamBest_SSD_Shell.cpp
+++ TeamBest_SSD_Shell/TeamBest_SSD_Shell.cpp
@@ -9,13 +9,13 @@ public:
     virtual void PrintHelp() = 0;
     virtual bool ReadInputFile(std::string inout, int address) = 0;  // File Read
     virtual bool WriteOutputFile(std::string output, int address) = 0;  // File Write
-    virtual void ProcessInputCommand(std::string inout) = 0;  // Command Input Processing
-    virtual bool ProcessParseInvalid(std::string inout) = 0;  // Command Parsing & invalid 처리
+    virtual void ProcessInputCommand(std::string command) = 0;  // Command Input Processing
+    virtual bool ProcessParseInvalid(std::string command) = 0;  // Command Parsing & invalid 처리
 
 private:
     const int MAX_LBA = 100;
-    virtual bool IsValidLba(int lba) = 0;  // LBA 유효성 검사
-    virtual bool IsValidValue(const std::string& value) = 0;  // VALUE 유효성 검사: 0x + 8자리 HEX
+    bool IsValidLba(int lba);  // LBA 유효성 검사
+    bool IsValidValue(const std::string& value);  // VALUE 유효성 검사: 0x + 8자리 HEX
 };
 
 
