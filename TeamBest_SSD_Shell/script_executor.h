8039b47dc7e7d8c3f40f8c8e859896949f74eaf8
eunshil.jeon <eunshil.jeon@gmail.com>
2025-05-12 18:34:26 +0900

    [Feature] test script 기능 구현 (ssd.exe 직접 호출 이용함)
    변경점
    1. tc 리스트는 testscript_all.hl 에 헤더파일을 추가하면된다.
    2. script 용 tc 구현은 tc 별로 각각 FullWriteAndReadCompare.h, PartialLBAWrite.h, WriteReadAging.h 에 구현되어 있다.
    visual studio 에 필터를 따로 추가하여 분리시켰다.
    네이밍에 대소문자가 섞여서 좀 안맞긴 하지만 교안에 있는 이름 동일하게 맞춰놨음.
    tc 가 추가되면 ScriptExcutor() 생성자 안에 registerCommand() 를 통해 등록시키기만 하면 됨
    3. script_command.h 에 라이브러리함수처럼 쓸 수 있도록 ReadCompard 추가함
    4. ssd.exe 파일은 SSD 프로젝트에 sha ba42b19 버전임
    
    검증
    
    [ v] Build
    
    [ v] Regression Test Pass
    
    [ v] 해당 기능 TC

---
+++ TeamBest_SSD_Shell/script_executor.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <iostream>
+#include "script_command.h"
+#include "testscript_all.hl"
+
+class ScriptExcutor {
+public:
+    ScriptExcutor() {
+        registerCommand("1_", std::unique_ptr<ScriptCommand>(new FullWRiteAndReadCompare()));
+        registerCommand("2_", std::unique_ptr<ScriptCommand>(new PartialLBAWrite()));
+        registerCommand("3_", std::unique_ptr<ScriptCommand>(new WriteReadAging()));
+    }
+
+    void registerCommand(const std::string& prefix, std::unique_ptr<ScriptCommand> command) {
+        commandMap[prefix] = std::move(command);
+    }
+
+    bool execute(const std::string& userInput) {
+        for (auto it = commandMap.begin(); it != commandMap.end(); ++it) {
+            const std::string& prefix = it->first;
+            ScriptCommand* command = it->second.get();
+
+            if (userInput.find(prefix) == 0) {
+                command->execute();
+                return true;
+            }
+        }
+
+        std::cout << "Unknown command: " << userInput << std::endl;
+        return false;
+    }
+
+private:
+    std::unordered_map<std::string, std::unique_ptr<ScriptCommand>> commandMap;
+};
