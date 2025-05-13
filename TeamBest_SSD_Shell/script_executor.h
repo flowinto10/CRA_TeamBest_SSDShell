e9e53d6ce8cd6c90900f0b97d09a7a8f37885fba
eunshil.jeon <eunshil.jeon@gmail.com>
2025-05-13 14:14:17 +0900

    [combine] bug fix. 복붙으로 만들다보니 불필요한 객체생성된 것 삭제
    exe file 추가(릴리즈 모드)
    
    [Feature] shell runner 실행 방식 수정
    
    변경점 : main.cpp() 안에서 인풋 아규먼트가 txt 로 추가가 더 들어오는 경우는 RUN() 을 돌지 않고 그냥 전체 script 실행을 하는 ExecuteAll() 호출 방식으로 변경함
    추가로 big loop 안에서 exit 가 먹히지 않는 오류 같이 수정함
    release mode big loop 안에서 5회 테스트용 수행 코드는 _DEBUG 로 처리함
    
    검증
    
    [ v] Build
    
    [ v] Regression Test Pass
    
    [ v] 해당 기능 TC + 릴리즈 모드에서 커맨드 직접 수행해봄
    
    [Feature] Runner 구현
    
    변경점
    1. script 쪽에서 executeAll 을 만들었음.shell shell_script.txt 를 입력하면 해당 파일에 적힌 tc 를 모조리 실행할 수 있도록 함
    2. release mode 에서 ProcessParseInvalid 이후에 검증된 command 는 NO_ERROR 처리해서 release mode exe 에서도 실행될 수 있도록 했음.
    3. shell_script.txt 에 현재 까지 작성된 script tc 목록을 추가하였음
    
    검증
    
    [ v] Build
    
    [ v] Regression Test Pass
    
    [ v] 해당 기능 TC

--- TeamBest_SSD_Shell/script_executor.h
+++ TeamBest_SSD_Shell/script_executor.h
@@ -8,31 +8,64 @@
 
 class ScriptExcutor {
 public:
-    ScriptExcutor() {
-        registerCommand("1_", std::unique_ptr<ScriptCommand>(new FullWRiteAndReadCompare()));
-        registerCommand("2_", std::unique_ptr<ScriptCommand>(new PartialLBAWrite()));
-        registerCommand("3_", std::unique_ptr<ScriptCommand>(new WriteReadAging()));
-    }
+	ScriptExcutor() {
+		registerCommand("1_", std::unique_ptr<ScriptCommand>(new FullWRiteAndReadCompare()));
+		registerCommand("2_", std::unique_ptr<ScriptCommand>(new PartialLBAWrite()));
+		registerCommand("3_", std::unique_ptr<ScriptCommand>(new WriteReadAging()));
+	}
 
-    void registerCommand(const std::string& prefix, std::unique_ptr<ScriptCommand> command) {
-        commandMap[prefix] = std::move(command);
-    }
+	void registerCommand(const std::string& prefix, std::unique_ptr<ScriptCommand> command) {
+		commandMap[prefix] = std::move(command);
+	}
 
-    bool execute(const std::string& userInput) {
-        for (auto it = commandMap.begin(); it != commandMap.end(); ++it) {
-            const std::string& prefix = it->first;
-            ScriptCommand* command = it->second.get();
+	bool execute(const std::string& userInput) {
+		for (auto it = commandMap.begin(); it != commandMap.end(); ++it) {
+			const std::string& prefix = it->first;
+			ScriptCommand* command = it->second.get();
 
-            if (userInput.find(prefix) == 0) {
-                command->RunScript();
-                return true;
-            }
-        }
+			if (userInput.find(prefix) == 0) {
+				command->RunScript();
+				return true;
+			}
+		}
 
-        std::cout << "Unknown command: " << userInput << std::endl;
-        return false;
-    }
+		std::cout << "Unknown command: " << userInput << std::endl;
+		return false;
+	}
+
+	bool ExecuteAll(const std::string& filename) { 
+		// shell_script.txt 를 읽어서 그 안에 있는 tc를 순서대로 실행함
+
+		std::string inputCommand = "";
+		std::ifstream file(filename);
+		bool ret = false;
+		if (file.is_open()) {
+			// 루프를 돌면서 한줄씩 읽은걸 ScriptExcutor 를 통해 실행한다.
+			while (std::getline(file, inputCommand)) {
+				// BOM 제거
+				if (inputCommand.compare(0, 3, "\xEF\xBB\xBF") == 0)
+					inputCommand = inputCommand.substr(3);
+				// CR 제거 (Windows에서 생기는 문제)
+				inputCommand.erase(std::remove(inputCommand.begin(), inputCommand.end(), '\r'), inputCommand.end());
+				// 공백 제거
+				inputCommand.erase(std::remove_if(inputCommand.begin(), inputCommand.end(), ::isspace), inputCommand.end());
+
+				cout << inputCommand << "___ Run... ";
+				ret = execute(inputCommand);
+
+				if (ret == false) { 
+					cout << "FAIL!!\n";
+					return false; // tc 실패시 즉시 종료
+				} 
+				cout << "Pass\n";
+			}
+		}
+		else {
+			std::cerr << "Unable to open file" << std::endl;
+			return false;
+		}
+	}
 
 private:
-    std::unordered_map<std::string, std::unique_ptr<ScriptCommand>> commandMap;
+	std::unordered_map<std::string, std::unique_ptr<ScriptCommand>> commandMap;
 };
